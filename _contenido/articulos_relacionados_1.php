67073f20cd4eeb8a72b57f1f39d25f2df742b365
mayaibuki <mayaibuki@hotmail.com>
2015-05-05 08:41:47 -0700

    Bigger thumbnails and links

--- _contenido/articulos_relacionados_1.php
+++ _contenido/articulos_relacionados_1.php
@@ -3,33 +3,33 @@
         <div class="row">
         <h3 class="section-heading">Artículos relacionados</h3>
         
-            <div class="col-xs-6 col-sm-4 col-md-2 animated pulse">
+            <div class="col-xs-6 col-md-4 animated pulse">
             	<a class="page-scroll" href="../condon"><div class="otros_imgbg" style="background-image: url('../_img_articulos/condon_sm.jpg');"><h5>¿Cómo usar un condón?</h5>
        			</div></a>
             </div>
         
-            <div class="col-xs-6 col-sm-4 col-md-2 animated pulse">
+            <div class="col-xs-6 col-md-4 animated pulse">
             	<a class="page-scroll" href="../histeria"><div class="otros_imgbg" style="background-image: url('../_img_articulos/hysteria_sm.jpg');"><h5>La Histeria Femenina</h5>
        			</div></a>
             </div>
             
-            <div class="col-xs-6 col-sm-4 col-md-2 animated pulse">
+            <div class="col-xs-6 col-md-4 animated pulse">
             	<a class="page-scroll" href="../sexo-consensual"><div class="otros_imgbg" style="background-image: url('../_img_articulos/consenso_sm.jpg');"><h5>¿Qué es el sexo con consentimiento?</h5>
        			</div></a>
             </div>
 
 
-            <div class="col-xs-6 col-sm-4 col-md-2 animated pulse">
+            <div class="col-xs-6 col-md-4 animated pulse">
             	<a class="page-scroll" href="../salud-con-juguetes"><div class="otros_imgbg" style="background-image: url('../_img_articulos/lelogigi_sm.jpg');"><h5>¿Cómo mejorar tu salud con juguetes?</h5>
        			</div></a>
             </div>
 
-            <div class="col-xs-6 col-sm-4 col-md-2 animated pulse">
+            <div class="col-xs-6 col-md-4 animated pulse">
             	<a href="../que-vemos-chris-grey"><div class="otros_imgbg" style="background-image: url('../_img_articulos/grey_reading_sm.jpg'); height:100%;"><h5>¿Qué vemos en Christian Grey?</h5>
        			</div></a>
             </div>            
             
-            <div class="col-xs-6 col-sm-4 col-md-2 animated pulse">
+            <div class="col-xs-6 col-md-4 animated pulse">
             	<a href="../6-fantasias-femeninas"><div class="otros_imgbg" style="background-image: url('../_img_articulos/fantasias_sm.jpg'); height:100%;"><h5>Las 6 fantasías de las mujeres</h5>
        			</div></a>
             </div>
--- _contenido/promoted_products_home.php
+++ _contenido/promoted_products_home.php
@@ -1,33 +1,33 @@
     <div class="container">
         <div class="row">        
 
-            <div class="col-xs-6 col-sm-4 col-md-2 animated pulse">
+            <div class="col-xs-6 col-md-4 animated pulse">
             	<a class="page-scroll" href="sexo-consensual"><div class="otros_imgbg" style="background-image: url('_img_articulos/consenso_sm.jpg');"><h5>¿Qué es el sexo con consentimiento?</h5>
        			</div></a>
             </div>
                         
-            <div class="col-xs-6 col-sm-4 col-md-2 animated pulse">
+            <div class="col-xs-6 col-md-4 animated pulse">
             	<a class="page-scroll" href="histeria"><div class="otros_imgbg" style="background-image: url('_img_articulos/hysteria_sm.jpg');"><h5>La Histeria Femenina</h5>
        			</div></a>
             </div>
 
 
-            <div class="col-xs-6 col-sm-4 col-md-2 animated pulse">
+            <div class="col-xs-6 col-md-4 animated pulse">
             	<a class="page-scroll" href="salud-con-juguetes"><div class="otros_imgbg" style="background-image: url('_img_articulos/lelogigi_sm.jpg');"><h5>¿Cómo mejorar tu salud con juguetes?</h5>
        			</div></a>
             </div>
 
-            <div class="col-xs-6 col-sm-4 col-md-2 animated pulse">
+            <div class="col-xs-6 col-md-4 animated pulse">
             	<a href="que-vemos-chris-grey"><div class="otros_imgbg" style="background-image: url('_img_articulos/grey_reading_sm.jpg'); height:100%;"><h5>¿Qué vemos en Christian Grey?</h5>
        			</div></a>
             </div>            
             
-            <div class="col-xs-6 col-sm-4 col-md-2 animated pulse">
+            <div class="col-xs-6 col-md-4 animated pulse">
             	<a href="6-fantasias-femeninas"><div class="otros_imgbg" style="background-image: url('_img_articulos/fantasias_sm.jpg'); height:100%;"><h5>Las 6 fantasías de las mujeres</h5>
        			</div></a>
             </div>
 
-            <div class="col-xs-6 col-sm-4 col-md-2 animated pulse">
+            <div class="col-xs-6 col-md-4 animated pulse">
             	<a class="page-scroll" href="squirt"><div class="otros_imgbg" style="background-image: url('_img_articulos/lavar_sm.jpg');"><h5>Eyaculación Femenina, ¿Qué es eso?</h5>
        			</div></a>
             </div>
