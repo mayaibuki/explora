8e7e4c34fb0f934886fa71af55428c7f2fd3e174
mayaibuki <mayaibuki@hotmail.com>
2015-05-10 20:35:03 -0700

    Home back to normal after testing

--- _contenido/featured_home.php
+++ _contenido/featured_home.php
@@ -1,28 +1,28 @@
     <div id="home" class="container">
         
 		<div class="row featured ">
-        	<a href="derecho-a-sexualidad">
+        	<a href="condon">
 			    <div class="col-sm-8 animated pulse">   
-            		<div class="featured_left animated pulse" style="background-image: url('_img_articulos/derecho-a-sexualidad_sm.jpg'); height:100%;">
-            			<h3>Mujeres, explorar nuestra sexualidad es un derecho</h3>
-            			<p class="descripcion">Leer más</p>
+            		<div class="featured_left animated pulse" style="background-image: url('_img_articulos/condon2_sm.jpg'); height:100%;">
+            			<h3>¿Cómo se usa un condón?</h3>
+            			<p class="descripcion">Aquí encontraras todo lo que necesitas saber para usarlo de forma apropiada</p>
 					</div>
 			</a>
        	</div>
        			
 		    <div class="col-sm-4">
-<!--            	<a href="comprar/we-vibe-4">
+            	<a href="comprar/we-vibe-4">
             		<div class="featured_right animated pulse" style="background-image: url('comprar/we-vibe-4/we-vibe-4.jpg'); height:100%;">
             			<h3>We Vibe 4</h3>
             			<p p class="descripcion">Vibrador para parejas</p>
 					</div>
-				</a>-->
-        	<a href="condon">
+				</a>
+        	<!--<a href="condon">
         		<div class="featured_right animated pulse" style="background-image: url('_img_articulos/condon2_sm.jpg'); height:100%;">
         			<h3>¿Cómo se usa un condón?</h3>
         			<p class="descripcion">Aquí encontraras todo lo que necesitas saber para usarlo de forma apropiada.</p>
 				</div>	
-        	</a>
+        	</a>-->
 								
 				
 		    </div>
